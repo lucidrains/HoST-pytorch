451964dd6d20a9af7a6bcfe2f4d7f77b38612432
lucidrains <lucidrains@gmail.com>
2025-03-14 10:02:19 -0700

    bring in a grouped mlp using einmix for the multiple critics, so that values can be all calculated in one pass

--- host_pytorch/host.py
+++ host_pytorch/host.py
@@ -6,6 +6,9 @@ from torch.nn import Module, ModuleList, Linear
 
 from hl_gauss_pytorch import HLGaussLoss
 
+from einops import repeat
+from einops.layers.torch import EinMix as Mix
+
 # helper functions
 
 def exists(v):
@@ -39,6 +42,46 @@ class MLP(Module):
         layers = ModuleList([Linear(dim_in, dim_out) for dim_in, dim_out in dim_pairs])
 
         self.layers = layers
+
+    def forward(
+        self,
+        x
+    ):
+
+        for ind, layer in enumerate(self.layers, start = 1):
+            is_last = ind == len(self.layers)
+
+            x = layer(x)
+
+            if not is_last:
+                x = F.silu(x)
+
+        return x
+
+class GroupedMLP(Module):
+    def __init__(
+        self,
+        *dims,
+        num_mlps = 1,
+    ):
+        super().__init__()
+
+        assert len(dims) >= 2, 'must have at least two dimensions'
+
+        dim_pairs = list(zip(dims[:-1], dims[1:]))
+
+        # handle first layer as no grouped dimension yet
+
+        first_dim_in, first_dim_out = dim_pairs.pop(0)
+
+        first_layer = Mix('b ... i -> b ... g o', weight_shape = 'g i o', bias_shape = 'g o', g = num_mlps, i = first_dim_in, o = first_dim_out)
+
+        # rest of the layers
+
+        layers = [Mix('b ... g i -> b ... g o', weight_shape = 'g i o', bias_shape = 'g o', g = num_mlps, i = dim_in, o = dim_out) for dim_in, dim_out in dim_pairs]
+
+        self.layers = ModuleList([first_layer, *layers])
+        self.num_mlps = num_mlps
 
     def forward(
         self,
